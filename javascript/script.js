c04710912c64fdc156a99e46ec0d6471b322f276
MuhammadA-C <phantomganhdi@gmail.com>
2023-07-20 15:19:43 -0700

    Single Round
    Created the code needed for the player to be able to play a single round of rock, paper, scissors against the computer.

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,105 @@
-let number = generateRandomNumber(3);
-console.log(getComputerChoice(number));
+/*
+Name: Muhammad
+Course: The Odin Project - Foundations
+Date: 07/20/2023
+  
+Overview: This is version 1 of the Rock, Paper, and Scissors Project in 
+The Odin Project Foundations course. 
+
+For this project you're supposed to create a rock, paper, and scissors console game
+where the player can play against the computer. 
+*/
+
+//Funciton to output "Rock" as a string to minimize the posibility of typos
+function getRock() {
+  return "Rock";
+}
+
+//Funciton to output "Paper" as a string to minimize the posibility of typos
+function getPaper() {
+  return "Paper";
+}
+
+//Funciton to output "Scissors" as a string to minimize the posibility of typos
+function getScissors() {
+  return "Scissors";
+}
 
 //Generates a random number from 1 to x
 function generateRandomNumber(upperBound) {
   return Math.floor(Math.random() * upperBound) + 1;
 }
 
+//Gets the computer choice by taking a number from 1 to 3 then outputs the correspodning rock, paper, or scissors
 function getComputerChoice(number) {
   switch (number) {
     case 1:
-      return "Rock";
+      return getRock();
     case 2:
-      return "Paper";
+      return getPaper();
     case 3:
-      return "Scissors";
+      return getScissors();
     default:
       return "Error number is out of bounds. Numbers can only be 1 to 3";
+  }
+}
+
+//Contains the logicx to decide who wins based on rock, paper, scissors
+function didYouWin(player1, player2) {
+  if (
+    player1 === getRock().toLowerCase() &&
+    player2 === getScissors().toLowerCase()
+  ) {
+    return true;
+  } else if (
+    player1 === getPaper().toLowerCase() &&
+    player2 === getRock().toLowerCase()
+  ) {
+    return true;
+  } else if (
+    player1 === getScissors().toLowerCase() &&
+    player2 === getPaper().toLowerCase()
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Outputs the winning choice. Ex: Rock beats Scissors
+function winningChoice(player1, player2) {
+  if (
+    player1 === getRock().toLowerCase() &&
+    player2 === getScissors().toLowerCase()
+  ) {
+    return `${getRock()} beats ${getScissors()}`;
+  }
+
+  if (
+    player1 === getPaper().toLowerCase() &&
+    player2 === getRock().toLowerCase()
+  ) {
+    return `${getPaper()} beats ${getRock()}`;
+  }
+
+  if (
+    player1 === getScissors().toLowerCase() &&
+    player2 === getPaper().toLowerCase()
+  ) {
+    return `${getScissors()} beats ${getPaper()}`;
+  }
+}
+
+//Plays a single round of rock, paper, scissors
+function playRound(computerSelection, playerSelection) {
+  let computer = computerSelection.toLowerCase();
+  let player = playerSelection.toLowerCase();
+
+  if (didYouWin(computer, player)) {
+    return `You Lose! ${winningChoice(computer, player)}`;
+  } else if (didYouWin(player, computer)) {
+    return `You Win! ${winningChoice(player, computer)}`;
+  } else {
+    return `Error: Incorrect input. Player input was: "${player}", Computer input was: "${computer}"`;
   }
 }
